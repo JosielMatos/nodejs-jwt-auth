a05ff2ffd7ccf57de3674361ba0c8e620bdc1308
JosielMatos <josielmatos77@protonmail.com>
2021-09-17 09:22:28 -0300

    password hash

--- models/User.js
+++ models/User.js
@@ -1,9 +1,29 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   password: String,
+});
+
+//Password hash middleware
+UserSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 
 router.get("/", (req, res) => {
@@ -7,7 +9,23 @@ router.get("/", (req, res) => {
 })
 
 router.post("/register", (req, res) => {
-  const { username, password } = req.body;
+  // const { username, password } = req.body;
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  bcrypt.hash(user.password, 10).then((hash) => {
+    user.create({
+      username: username,
+      password: hash,
+    })  // hash on model ???
+  }).then(() => {
+    res.json('Done! User registered!')
+  }).catch(err => {
+    if (err) res.status(400).json({error: err});
+  })
 });
 
 router.post("/login", (req, res) => {
