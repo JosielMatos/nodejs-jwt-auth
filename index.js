2766c80499f06a5a9bd2d890fb0b4feb07c00594
JosielMatos <josielmatos77@protonmail.com>
2021-10-06 16:51:09 -0300

    Add logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const logger = require("morgan");
 const routes = require("./routes/routes");
 const cookieParser = require("cookie-parser");
 
@@ -9,6 +10,9 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+
+//logging
+app.use(logger("dev"));
 
 const PORT = process.env.PORT || 5000;
 
